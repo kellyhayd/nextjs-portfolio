8163196c35d68c1cfb70f0f265f5b212379553ac
Kelly Hayd <hayd.kelly@gmail.com>
2024-01-03 15:20:44 -0300

    contact ok

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import GithubIcon from "../../../public/github-icon.svg";
+import InstagramIcon from "../../../public/ig-icon.svg";
 import LinkedinIcon from "../../../public/linkedin-icon.svg";
 import Link from "next/link";
 import Image from "next/image";
@@ -44,7 +44,7 @@ const EmailSection = () => {
       id="contact"
       className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
     >
-      <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
+      <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-secondary to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
         <h5 className="text-xl font-bold text-white my-2">
           Vamos Conversar?
@@ -55,9 +55,9 @@ const EmailSection = () => {
           Se você tem alguma pergunta, deseja dizer olá ou está interessado em colaborar, ficarei feliz em
           responder da melhor forma possível!
         </p>
-        <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
-            <Image src={GithubIcon} alt="Github Icon" />
+        <div className="socials flex flex-row gap-1">
+          <Link href="https://www.instagram.com/kellyhayd">
+            <Image src={InstagramIcon} alt="Instagram Icon" />
           </Link>
           <Link href="linkedin.com">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
