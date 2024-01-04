e901aaa8f4f74b0470fb47476de3b9b66060958d
Kelly Hayd <hayd.kelly@gmail.com>
2024-01-03 22:28:40 -0300

    hero with ia image

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -49,13 +49,13 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="rounded-full bg-[#181818] w-[200px] h-[200px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/hero-image.png"
+              src="/images/hero-img-kh.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
+              width={350}
+              height={350}
             />
           </div>
         </motion.div>
