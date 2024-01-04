257002484e3fb25a10e31b13db25b16f18551fc1
Kelly Hayd <hayd.kelly@gmail.com>
2024-01-03 22:31:23 -0300

    modal appearence config

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -10,18 +10,35 @@ const ProjectCard = ({ imgUrl, title, description, vimeoUrl, previewUrl }) => {
         style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
       >
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
-          <Link
+          {/* <Link
             href={vimeoUrl}
             className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
           >
             <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
-          </Link>
-          <Link
+          </Link> */}
+          //link sem destino
+          {/* <Link
             href={previewUrl}
             className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
           >
             <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
-          </Link>
+          </Link> */}
+          <button data-modal-target="#modal">
+            {/* class="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"  */}
+            <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+          </button>
+          <div class="modal" id="modal">
+            <div class="modal-header">
+              <div class="title">Var-Título</div>
+              <button data-close-button class="close-button">&times;</button>
+            </div>
+            <div class="modal-body">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Possimus iure rerum quidem fugiat beatae porro harum magni sint
+              id obcaecati reprehenderit hic mollitia illo, et at similique adipisci fugit vitae!
+            </div>
+          </div>
+          <div class="active" id="overlay"></div>
         </div>
       </div>
       <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
